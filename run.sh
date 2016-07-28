32068ee5c0e1bac851bd8a9ecd6b091f7fcfb5d1
Jeff Deskins <Jeff.Deskins@lifeway.com>
2016-07-28 16:29:42 -0500

    Mount volume to host to save kube config

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 # env-file is always processed first no matter the order
 docker run -it --env-file ./env.list -e USER -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION \
     -p 8001:8001 \
+    -v $(PWD)/kubeconfig:/root/.kube/ \
     jdeskins/kubernetes-aws
