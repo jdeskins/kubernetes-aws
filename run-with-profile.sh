ba193b8ba7db872c486206b77236334c0406587b
Jeff Deskins <Jeff.Deskins@lifeway.com>
2016-08-03 14:42:15 -0500

    Changed zone; Run with AWS profile;

---
+++ run-with-profile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# env-file is always processed first no matter the order
+docker run -it --env-file ./env.list -e USER -e AWS_DEFAULT_PROFILE -e AWS_DEFAULT_REGION \
+    -p 8001:8001 \
+    -v $(PWD)/kubeconfig:/root/.kube/ \
+    -v ~/.aws:/root/.aws \
+    jdeskins/kubernetes-aws
